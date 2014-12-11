8931bb50e813cbc694646b438d24d9ddf5a7cff0
dtch <dtch.nix@gmail.com>
2014-12-10 19:54:43 -0500

    Overhaul to site version

--- code/temp.py
+++ code/temp.py
@@ -1,22 +1,25 @@
 # dtch.info
-# Version: 0.01
+# Current Version: 0.06
 # converts temperatures between celsius and farenheit
 # http://dtch.info/code/temp.py
+# Changelog:
+# 0.06  - switched from int to float for more precise
+#       measurements
+# 0.05  - %s/far*/farenheit
+# 0.04  - simplified code (thanks to pulse on ##programming)
+# 0.03  - fixed issue #001: temperatures converting to celsius coming out wrong
+# 0.02  - print statements changed
+# 0.01  - Initial Code
 
-temp = int(input("Temperature: "))
+temp = float(input("Temperature: "))
 
 conv = raw_input("Convert to Farenheit (f) or Celsius (c)?\n")
 
 if conv == "c":
-    print "Converting %d to celsius" % temp
-    ctemp0 = temp - 32
-    ctemp1 = ctemp0 / 5
-    ctemp2 = ctemp1 / 9
-    print "The new temp is %d" % ctemp2
+    print "Converting %d farenheit to celsius" % temp
+    ctemp = (temp - 32.0) / 1.8
+    print "The new temp is %d celsius" % ctemp
 elif conv == "f":
-    print "Converting %d to farneheit" % temp
-    ftemp0 = temp * 9
-    ftemp1 = ftemp0 / 5
-    ftemp2 = ftemp1 + 32
-    print "The new temp is %d" %ftemp2
-    
+    print "Converting %d celsius to farenheit" % temp
+    ftemp = (temp * 1.8) + 32.0
+    print "The new temp is %d farenheit" % ftemp 
